550e3bfa7e989aac3d74d37871543da30aeef9cc
millimon-99 <mil31limon@gmail.com>
2020-02-22 18:06:13 -0600

    Versión 1.1

--- Proyecto_1.pl
+++ Proyecto_1.pl
@@ -1,5 +1,20 @@
 %Si el tablero es nxn con números entre 0 y 9
-tableroValido(Tablero).
+tamanoR([],1).
+tamanoR([_|Z],N):-
+  tamanoR(Z,N1), N is N1+1.
+tamanoC([],0).
+tamanoC([_|Z],N):-
+  tamanoC(Z,N1), N is N1+1.
+listaConNumUnDigito([]).
+listaConNumUnDigito([L|L1]):-
+  L/10==0,listaConNumUnDigito(L1).
+tableroValido([],0).
+tableroValido([T|T1],NumRow,NumCol):-
+  tamanoR(T1,NumRow),
+  tamanoC(T,NumCol),
+  NumCol==NumRow,
+  tableroValido(T1,NumRow2,NumCol).
+
 %Si en ListaMov y ListaNums tienen todas las soluciones
 %de Tablero, partiedn de la fila X, columna Y y de n saltos
 saltosEnPosicion(Tablero,X,Y,ListaMov,ListaNums).
@@ -11,4 +26,3 @@ salto(Tablero,ListaMov,ListaNums).
 saltosTablero(Tablero,ListaMov,ListaNums).
 % +extra
 solucion(Saltos,X,Y,Movimientos):-
-  
